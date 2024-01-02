eb205a1a25e40bade9695f53225aee3727e69ef1
Danny Cheun <dcheun@gmail.com>
2024-01-02 21:13:12 +0800

    feat: Payload, tRPC
    Add tRPC packages, routes, and configs.
    Add nodemailer packages.
    Additional payload configuration.
    Add support to send onboarding email on sign-up.

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -9,6 +9,7 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from '@/lib/validators/account-credentials-validators';
+import { trpc } from '@/trpc/client';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
@@ -23,8 +24,10 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-    // send data to the server
+    mutate({ email, password });
   };
 
   return (
@@ -64,6 +67,7 @@ const Page = () => {
                   <Label htmlFor="password">Password</Label>
                   <Input
                     {...register('password')}
+                    type="password"
                     className={cn({
                       'focus-visible:ring-red-500': errors.password,
                     })}
